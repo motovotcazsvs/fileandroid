f9e81961638c1f7cd85ea4035a3fa6d1a57bd969
motovotcazsvs <motovotcazsvs@gmail.com>
2024-12-03 22:24:39 +0200

    sorting functions

--- fileoperations.cpp
+++ fileoperations.cpp
@@ -69,44 +69,10 @@ bool FileOperations::hasSingleUri() const
     return m_uri.isValid() && DocumentsContract::isDocumentUri(m_uri) && (!m_tree || m_uri != m_tree->uri());
 }
 
-bool FileOperations::hasTreeUri() const
-{
-    return m_tree.get();
-}
-
-bool FileOperations::hasParent() const
-{
-    return m_tree && m_tree->parent();
-}
-
 QByteArray FileOperations::fileContent() const
 {
     return hasSingleUri() ? ContentResolver::instance().openUri(m_uri, QIODevice::ReadOnly)->readAll()
                           : QByteArray{};
-}
-
-void FileOperations::newFile(const QString &fileName)
-{
-    SharedStorage::instance().createDocument(1,
-        [this](int code, net::Uri uri){
-            if (code != 1 || !uri.isValid())
-                return;
-            ContentResolver::instance().takePersistableRWUriPermissions(uri);
-            openTree();
-            openUri(uri);
-        }, "application/octet-stream", fileName);
-}
-
-void FileOperations::openFile(const QStringList &mimeTypes)
-{
-    SharedStorage::instance().openDocument(2,
-        [this](int code, net::Uri uri){
-            if (code != 2 || !uri.isValid())
-                return;
-            ContentResolver::instance().takePersistableRWUriPermissions(uri);
-            openTree();
-            openUri(uri);
-        }, mimeTypes);
 }
 
 void FileOperations::openDir()
@@ -130,12 +96,6 @@ void FileOperations::saveContent(const QByteArray &content)
 void FileOperations::newTreeFile(const QString &name)
 {
     m_tree->createFile("application/octet-stream", name);
-    openTree(m_tree);
-}
-
-void FileOperations::newTreeFolder(const QString &name)
-{
-    m_tree->createDirectory(name);
     openTree(m_tree);
 }
 
@@ -165,6 +125,7 @@ void FileOperations::openTree(const DocumentFilePtr &tree)
 void FileOperations::openUri(const net::Uri &uri)
 {
     m_uri = uri;
+    QByteArray arr = fileContent();
 }
 
 QString FileOperations::fileName() const
@@ -174,7 +135,51 @@ QString FileOperations::fileName() const
                            : QString{};
 }
 
+
+
+
+
 QString FileOperations::url() const
 {
     return m_uri.isValid() ? m_uri.toString() : QString{};
+}
+
+bool FileOperations::hasTreeUri() const
+{
+    return m_tree.get();
+}
+
+bool FileOperations::hasParent() const
+{
+    return m_tree && m_tree->parent();
+}
+
+void FileOperations::newTreeFolder(const QString &name)
+{
+    m_tree->createDirectory(name);
+    openTree(m_tree);
+}
+
+void FileOperations::newFile(const QString &fileName)
+{
+    SharedStorage::instance().createDocument(1,
+                                             [this](int code, net::Uri uri){
+                                                 if (code != 1 || !uri.isValid())
+                                                     return;
+                                                 ContentResolver::instance().takePersistableRWUriPermissions(uri);
+                                                 openTree();
+                                                 openUri(uri);
+                                             }, "application/octet-stream", fileName);
+}
+
+void FileOperations::openFile(const QStringList &mimeTypes)
+{
+    SharedStorage::instance().openDocument(2,
+                                           [this](int code, net::Uri uri){
+                                               if (code != 2 || !uri.isValid())
+                                                   return;
+                                               ContentResolver::instance().takePersistableRWUriPermissions(uri);
+                                               openTree();
+                                               openUri(uri);
+                                           }, mimeTypes);
 }
